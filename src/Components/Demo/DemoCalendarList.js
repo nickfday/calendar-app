8ad9b0021c84f1b4331c3320b2f2ab352c9358d4
Nick Finley-Day <nickfday@gmail.com>
2017-08-08 00:46:14 +0100

    Working pagination with filter and dynamic pager length

--- src/Components/Demo/DemoCalendarList.js
+++ src/Components/Demo/DemoCalendarList.js
@@ -3,52 +3,6 @@ import { searchFilter, filterMultiSelect } from "../Misc/Helper";
 import { DemoCalendarRow } from "./DemoCalendarRow";
 import moment from "moment";
 import Pagination from "react-js-pagination";
-import ReactPaginate from "react-paginate";
-
-const EventFilters = props => {
-  //Search by Name
-  if (!searchFilter(props.eventState.titleText, props.event.title)) {
-    return false;
-  }
-
-  //Search by Address
-  if (!searchFilter(props.eventState.addressText, props.event.location)) {
-    return false;
-  }
-
-  //Event filter
-  if (
-    !filterMultiSelect(
-      props.eventState.selectedEventTypes,
-      props.event.event_type
-    )
-  )
-    return false;
-
-  //Audience Filter
-  if (
-    !filterMultiSelect(
-      props.eventState.selectedAudienceTypes,
-      props.event.audience
-    )
-  )
-    return false;
-
-  //Date Filter
-  if (
-    moment(props.eventState.startDate) > moment(props.startDate) ||
-    moment(props.eventState.endDate) < moment(props.endDate)
-  )
-    return false;
-
-  return (
-    <DemoCalendarRow
-      event={props.event}
-      startDate={props.startDate}
-      endDate={props.endDate}
-    />
-  );
-};
 
 class DemoCalendarList extends Component {
   constructor(props) {
@@ -82,40 +36,62 @@ class DemoCalendarList extends Component {
       });
     });
 
-    dateArray.sort(function(a, b) {
-      return moment(a[0][0]) < moment(b[0][0]);
-    });
-
     // list events
     let activePage = this.state.activePage;
     let itemsCountPerPage = 5;
 
-    let listEvents = dateArray
-      .sort()
-      .slice(
-        activePage * itemsCountPerPage - itemsCountPerPage,
-        activePage * itemsCountPerPage
+    function filterEvents(eventState, event, startDate, endDate) {
+      //this.handlePageChange(1);
+
+      if (!searchFilter(eventState.titleText, event.title)) return false;
+
+      //Search by Address
+      if (!searchFilter(eventState.addressText, event.location)) return false;
+
+      //Event filter
+      if (!filterMultiSelect(eventState.selectedEventTypes, event.event_type))
+        return false;
+
+      //Audience Filter
+      if (!filterMultiSelect(eventState.selectedAudienceTypes, event.audience))
+        return false;
+
+      //Date Filter
+      if (
+        moment(eventState.startDate) > moment(startDate) ||
+        moment(eventState.endDate) < moment(endDate)
       )
-      .map(i => {
-        return (
-          <div>
-            <EventFilters
-              eventState={this.props.eventState}
-              event={i[1]}
-              startDate={i[0][0]}
-              endDate={i[0][1]}
-            />
-          </div>
-        );
-      });
+        return false;
+
+      filteredEvents.push(
+        <DemoCalendarRow
+          event={event}
+          startDate={startDate}
+          endDate={endDate}
+        />
+      );
+    }
+
+    let filteredEvents = [];
+
+    let listEvents = dateArray.sort().map(i => {
+      filterEvents(this.props.eventState, i[1], i[0][0], i[0][1]);
+    });
+
+    let filteredEventsCount = filteredEvents.length;
+
+    filteredEvents = filteredEvents.slice(
+      activePage * itemsCountPerPage - itemsCountPerPage,
+      activePage * itemsCountPerPage
+    );
 
     return (
       <div>
-        {listEvents}
+        {filteredEvents}
         <Pagination
           activePage={this.state.activePage}
           itemsCountPerPage={5}
-          totalItemsCount={dateArray.length}
+          totalItemsCount={filteredEventsCount}
           pageRangeDisplayed={5}
           onChange={this.handlePageChange.bind(this)}
         />
