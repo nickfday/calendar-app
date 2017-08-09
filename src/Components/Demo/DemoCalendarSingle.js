807b23339234d0cabf4819dbfe00496dbbf1348b
Nick Finley-Day <nickfday@gmail.com>
2017-08-09 17:36:05 +0100

    Added single markup and handling and fetching conditional

--- src/Components/Demo/DemoCalendarSingle.js
+++ src/Components/Demo/DemoCalendarSingle.js
@@ -7,15 +7,74 @@ import { Link } from "react-router-dom";
 import moment from "moment";
 import { splitMap } from "../Misc/Helper";
 
+const DisplaySingleEvent = props => {
+  const item = props.event;
+  const startDate = props.startDate;
+  const endDate = props.endDate;
+
+  return (
+    <CSSTransitionGroup
+      component="div"
+      transitionName="row"
+      transitionAppear={true}
+      transitionAppearTimeout={500}
+      transitionLeaveTimeout={500}
+      transitionEnterTimeout={500}
+      className="event-row clearfix"
+    >
+      <div className="col-xs-2">
+        <div className="date-info">
+          <div className="custom-dayOfWeek">
+            {moment(startDate).format("ddd")}
+          </div>
+          <div className="custom-day">
+            {moment(startDate).format("D")}
+          </div>
+          <div className="custom-month">
+            {moment(startDate).format("MMM")}
+          </div>
+          <div className="custom-year">
+            {moment(startDate).format("YYYY")}
+          </div>
+        </div>
+      </div>
+
+      <div className="event-info col-xs-10">
+        <h3>
+          {props.event.title}
+        </h3>
+        <div>
+          {item.body}
+        </div>
+        <BSModal
+          buttonLabel={item.location}
+          map={
+            "https://www.google.com/maps/embed/v1/place?key=AIzaSyD8cbhTTREwAxNI3IxRLwMGfE1xb_eOINc&q=" +
+            item.location
+          }
+        />
+        {splitMap(item.event_type, ", ", "event-item")}
+        <br />
+        <div className="clearfix" />
+        {splitMap(item.audience, ", ", "audience-item")}
+      </div>
+      <Link to="/">Back to All Events</Link>
+    </CSSTransitionGroup>
+  );
+};
+
 class DemoCalendarSingle extends Component {
   constructor() {
     super();
     this.state = {
-      events: null
+      events: null,
+      fetchEvents: null,
+      startDate: null,
+      endDate: null
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     if (!this.props.location.state) {
       console.log("Fetch...");
       let pathUUID = this.props.location.pathname;
@@ -25,20 +84,25 @@ class DemoCalendarSingle extends Component {
       }
     } else {
       console.log("Props Found");
-      this.setItemValue(this.props.location.state);
+      this.setState({
+        events: this.props.location.state
+      });
+      //this.setItemValue(this.props.location.state, "events");
     }
   }
 
-  setItemValue(data) {
-    console.log("set item value");
-    console.log(data);
-    // this.setState((prevState, props) => ({
-    //   events: data
-    // }));
-    this.setState({
-      events: data
-    });
-  }
+  // setItemValue(data, stateObject) {
+  //   console.log("set item value");
+  //   console.log(data);
+  //   // this.setState((prevState, props) => ({
+  //   //   events: data
+  //   // }));
+  //   this.setState({
+  //     stateObject: data
+  //   });
+
+  //   console.log(this.state);
+  // }
 
   fetchSingleEvent(UUID) {
     //alert(UUID);
@@ -50,72 +114,70 @@ class DemoCalendarSingle extends Component {
           UUID
       )
       .then(response => {
+        this.setState({
+          fetchEvents: response.data
+        });
+        // self.setState((prevState, props) => ({
+        //   fetchEvents: response.data
+        // }));
+
         // self.setState((prevState, props) => ({
         //   events: response.data,
         //   loaded: true
         // }));
-        self.setItemValue(response.data);
+        //self.setItemValue(response.data, "fetchEvents");
       })
       .catch(function(error) {
         console.log(error);
+      })
+      .then(function(response) {
+        self.handleEventDate(self);
       });
-    // .then(function(response) {
+  }
 
-    // });
+  handleEventDate(self) {
+    //const self = this;
+    var updatedevents = self.state.fetchEvents.slice();
+    updatedevents.map(function(i) {
+      if (i.date_repeat) {
+        i.splitDates = [];
+        i.sortedDates = [];
+        i.splitDates.push(i.date_repeat.split(", "));
+        i.splitDates[0].map(function(y) {
+          i.sortedDates.push(y.split(" to "));
+          return null;
+        });
+        return null;
+      }
+      return null;
+    });
+
+    self.setState(() => ({
+      fetchEvents: updatedevents
+    }));
   }
 
   render() {
-    console.log(this.state);
-
-    return (
-      <CSSTransitionGroup
-        component="div"
-        transitionName="row"
-        transitionAppear={true}
-        transitionAppearTimeout={500}
-        transitionLeaveTimeout={500}
-        transitionEnterTimeout={500}
-        className="event-row clearfix"
-      >
-        <div className="col-xs-2">
-          <div className="date-info">
-            <div className="custom-dayOfWeek">
-              {moment(this.state.events.startDate).format("ddd")}
-            </div>
-            <div className="custom-day">
-              {moment(this.state.events.startDate).format("D")}
-            </div>
-            <div className="custom-month">
-              {moment(this.state.events.startDate).format("MMM")}
-            </div>
-            <div className="custom-year">
-              {moment(this.state.events.startDate).format("YYYY")}
-            </div>
-          </div>
-        </div>
-
-        <div className="event-info col-xs-7">
-          <h3>
-            {this.state.events.events.title}
-          </h3>
-          <div>
-            {this.state.events.events.body}
-          </div>
-          <BSModal
-            buttonLabel={this.state.events.events.location}
-            map={
-              "https://www.google.com/maps/embed/v1/place?key=AIzaSyD8cbhTTREwAxNI3IxRLwMGfE1xb_eOINc&q=" +
-              this.state.events.events.location
-            }
-          />
-          {splitMap(this.state.events.events.event_type, ", ", "event-item")}
-          <br />
-          <div className="clearfix" />
-          {splitMap(this.state.events.events.audience, ", ", "audience-item")}
-        </div>
-        <Link to="/">Back to All Events</Link>
-      </CSSTransitionGroup>
-    );
+    if (this.state.events !== null) {
+      return (
+        <DisplaySingleEvent
+          event={this.state.events.events}
+          startDate={this.state.events.startDate}
+          endDate={this.state.events.endDate}
+        />
+      );
+    } else if (this.state.fetchEvents !== null) {
+      console.log("Display Fetched...");
+      return (
+        <DisplaySingleEvent
+          event={this.state.fetchEvents[0]}
+          startDate={this.state.fetchEvents[0].sortedDates[0][0]}
+          endDate={this.state.fetchEvents[0].sortedDates[0][1]}
+        />
+      );
+    } else {
+      return <div>Loading...</div>;
+    }
   }
 }
 export default DemoCalendarSingle;
