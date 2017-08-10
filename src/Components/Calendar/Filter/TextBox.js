9b0c37cb2c45173e480b38da4c0bc5100a9b1e2a
Nick Finley-Day <nickfday@gmail.com>
2017-08-10 21:11:54 +0100

    fixed enter key submitting form

--- src/Components/Calendar/Filter/TextBox.js
+++ src/Components/Calendar/Filter/TextBox.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class TextBox extends Component {
-
   handleFilterTextInputChange(e) {
     this.props.onFilterTextInput(e.target.value);
   }
 
   render() {
-    return(
-       <div className="form-group">
-         <label className="sr-only" htmlFor="exampleInputEmail3">{this.props.placeholder}</label>
-         <input type="text"
-           className="form-control"
-           placeholder={this.props.placeholder}
-           value={this.props.filterText}
-           onChange={this.handleFilterTextInputChange.bind(this)}
-         />
-       </div>
+    //Prevent Enter from submitting
+    function handleKeyPress(e) {
+      if (e.key === "Enter") {
+        e.preventDefault();
+      }
+    }
+    return (
+      <div className="form-group">
+        <label className="sr-only" htmlFor="exampleInputEmail3">
+          {this.props.placeholder}
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder={this.props.placeholder}
+          value={this.props.filterText}
+          onChange={this.handleFilterTextInputChange.bind(this)}
+          onKeyPress={handleKeyPress}
+        />
+      </div>
     );
   }
 }
