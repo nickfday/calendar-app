735b59e7c7c3c8e61548b7cfa7bd7842f6014a81
Nick Finley-Day <nickfday@gmail.com>
2017-08-21 09:06:25 +0000

    added wcc logo markup

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 //import 'bootstrap/dist/css/bootstrap-theme.css';
-import './header.css'
+import "./header.css";
 //import 'https://www.westminster.gov.uk/sites/www.westminster.gov.uk/themes/wcc/css/main-sm.css?opu6pq';
 
-
 class Layout extends Component {
-
-	render() {
-		return (
-			 <div>
-			 <div className="top-bar"></div>
-			 </div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div className="top-bar">
+          <div className="container">
+            <div className="logo-wrap">
+              <a href="/" title="Home" className="logo icon-logo" />
+              <div className="switch icon-arrow-down hide-md-up" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Layout;
