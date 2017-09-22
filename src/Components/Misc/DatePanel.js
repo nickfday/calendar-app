041a77fef97ddc611c7c946f46e9640e83d29c4f
Nick Finley-Day <nickfday@gmail.com>
2017-09-22 10:44:38 +0000

    added date panel component

---
+++ src/Components/Misc/DatePanel.js
@@ -0,0 +1,18 @@
+import React from "react";
+import moment from "moment";
+import PropTypes from "prop-types";
+
+export function DatePanel(props) {
+  return (
+    <div className="date-info">
+      <div className="custom-dayOfWeek">{moment(props.date).format("ddd")}</div>
+      <div className="custom-day">{moment(props.date).format("D")}</div>
+      <div className="custom-month">
+        {moment(props.date).format("MMM")} {moment(props.date).format("YYYY")}
+      </div>
+    </div>
+  );
+  DatePanel.propTypes = {
+    date: PropTypes.instanceOf(Date)
+  };
+}
