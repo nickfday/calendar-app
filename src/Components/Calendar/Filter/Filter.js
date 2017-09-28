043b578f706d0d0384cc2b507745377ed4d02817
Nick Finley-Day <nickfday@gmail.com>
2017-09-28 15:59:29 +0000

    added accordion functionality to filters panel

--- src/Components/Calendar/Filter/Filter.js
+++ src/Components/Calendar/Filter/Filter.js
@@ -2,92 +2,93 @@ import React, { Component } from "react";
 import TextBox from "./TextBox";
 import MySelect from "./MySelect";
 import DatePicker from "react-datepicker";
+import { Accordion, Panel } from "react-bootstrap";
+
 //import moment from 'moment';
 import "react-datepicker/dist/react-datepicker.css";
 
 class Filter extends Component {
   render() {
     return (
-      <div className="panel panel-info">
-        <div className="panel-heading">
-          <h3 className="panel-title">Filter events</h3>
-        </div>
-        <div className="panel-body">
-          <form id="exerciseForm" className="calendar-form">
-            <TextBox
-              filterText={this.props.titleText}
-              onFilterTextInput={this.props.onTitleTextInput}
-              placeholder="Search by name"
-            />
-            <TextBox
-              filterText={this.props.addressText}
-              onFilterTextInput={this.props.onAddressTextInput}
-              placeholder="Search by address"
-            />
-
-            <div className="form-group">
-              <DatePicker
-                selectsStart
-                startDate={this.props.startDate}
-                endDate={this.props.endDate}
-                className="form-control"
-                selected={this.props.startDate}
-                onChange={this.props.handleStartDate}
-                placeholderText="Start date"
-                dateFormat="DD/MM/YYYY"
-                isClearable={true}
-                todayButton={"Today"}
+      <Accordion>
+        <Panel header="Filters" eventKey="1" bsStyle="info">
+          <div className="panel-body">
+            <form id="exerciseForm" className="calendar-form">
+              <TextBox
+                filterText={this.props.titleText}
+                onFilterTextInput={this.props.onTitleTextInput}
+                placeholder="Search by name"
               />
-            </div>
-
-            <div className="form-group">
-              <DatePicker
-                selectsEnd
-                startDate={this.props.startDate}
-                endDate={this.props.endDate}
-                className="form-control"
-                selected={this.props.endDate}
-                onChange={this.props.handleEndDate}
-                placeholderText="End date"
-                dateFormat="DD/MM/YYYY"
-                isClearable={true}
-                todayButton={"Today"}
+              <TextBox
+                filterText={this.props.addressText}
+                onFilterTextInput={this.props.onAddressTextInput}
+                placeholder="Search by address"
               />
-            </div>
 
-            <label className="sr-only" htmlFor="exampleInputEmail3">
-              Event Type
-            </label>
-            <MySelect
-              data={this.props.eventTypes}
-              selectedItems={this.props.calenderState.selectedEventTypes}
-              handleSelectedItem={this.props.handleSelectedEventTypes}
-              placeholder="Select event type"
-              multiSelect={true}
-              className="form-group event-select"
-            />
+              <div className="form-group">
+                <DatePicker
+                  selectsStart
+                  startDate={this.props.startDate}
+                  endDate={this.props.endDate}
+                  className="form-control"
+                  selected={this.props.startDate}
+                  onChange={this.props.handleStartDate}
+                  placeholderText="Start date"
+                  dateFormat="DD/MM/YYYY"
+                  isClearable={true}
+                  todayButton={"Today"}
+                />
+              </div>
 
-            <label className="sr-only" htmlFor="exampleInputEmail3">
-              Who it's for
-            </label>
-            <MySelect
-              data={this.props.calenderState.audienceTypes}
-              selectedItems={this.props.calenderState.selectedAudienceTypes}
-              handleSelectedItem={this.props.handleSelectedAudienceTypes}
-              placeholder="Select audience type"
-              className="form-group"
-              multiSelect={true}
-            />
+              <div className="form-group">
+                <DatePicker
+                  selectsEnd
+                  startDate={this.props.startDate}
+                  endDate={this.props.endDate}
+                  className="form-control"
+                  selected={this.props.endDate}
+                  onChange={this.props.handleEndDate}
+                  placeholderText="End date"
+                  dateFormat="DD/MM/YYYY"
+                  isClearable={true}
+                  todayButton={"Today"}
+                />
+              </div>
 
-            <button
-              onClick={this.props.handleReset}
-              className="btn btn-primary btn-wcc"
-            >
-              Reset
-            </button>
-          </form>
-        </div>
-      </div>
+              <label className="sr-only" htmlFor="exampleInputEmail3">
+                Event Type
+              </label>
+              <MySelect
+                data={this.props.eventTypes}
+                selectedItems={this.props.calenderState.selectedEventTypes}
+                handleSelectedItem={this.props.handleSelectedEventTypes}
+                placeholder="Select event type"
+                multiSelect={true}
+                className="form-group event-select"
+              />
+
+              <label className="sr-only" htmlFor="exampleInputEmail3">
+                Who it's for
+              </label>
+              <MySelect
+                data={this.props.calenderState.audienceTypes}
+                selectedItems={this.props.calenderState.selectedAudienceTypes}
+                handleSelectedItem={this.props.handleSelectedAudienceTypes}
+                placeholder="Select audience type"
+                className="form-group"
+                multiSelect={true}
+              />
+
+              <button
+                onClick={this.props.handleReset}
+                className="btn btn-primary btn-wcc"
+              >
+                Reset
+              </button>
+            </form>
+          </div>
+        </Panel>
+      </Accordion>
     );
   }
 }
