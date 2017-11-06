9c656f825fe2efe83fa1af3f3e0fd79f802ebb48
Nick Finley-Day <nickfday@gmail.com>
2017-11-06 12:03:15 +0000

    Added form element with submit Handler

--- src/Components/Form/SearchBar.js
+++ src/Components/Form/SearchBar.js
@@ -1,23 +1,23 @@
-import React, { Component } from "react";
-import "./style/SearchBar.css";
+import React, { Component } from 'react';
+import './style/SearchBar.css';
 //import "./westminster-main-sm.css";
 
 const SearchBarStyle = {
-  marginTop: "12px"
+  marginTop: '12px'
 };
 
 class SearchBar extends Component {
   constructor() {
     super();
     this.state = {
-      query: ""
+      query: ''
     };
 
     this.handleTitleTextInput = this.handleTitleTextInput.bind(this);
   }
 
   handleTitleTextInput(e) {
-    console.log("text change");
+    console.log('text change');
     this.setState({
       query: e.target.value
     });
@@ -26,29 +26,32 @@ class SearchBar extends Component {
   handleSubmit(e) {
     e.preventDefault();
     window.location =
-      "https://www.westminster.gov.uk/search?collection=wcc-web-website&query=" +
+      'https://www.westminster.gov.uk/search?collection=wcc-web-website&query=' +
       this.state.query;
   }
 
   render() {
     return (
       <div className="search-box search-box-small" style={SearchBarStyle}>
-        <input
-          type="text"
-          placeholder="What would you like to do?"
-          onChange={this.handleTitleTextInput.bind(this)}
-        />
-        <a
-          className="btn"
-          onClick={this.handleSubmit.bind(this)}
-          //onClick={e => this.handleSubmit(e, this.state.query)}
-          href=""
-        >
-          <span
-            className="glyphicon glyphicon-search search"
-            aria-hidden="true"
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input
+            type="text"
+            placeholder="What would you like to do?"
+            onChange={this.handleTitleTextInput.bind(this)}
           />
-        </a>
+          <a
+            className="btn"
+            onClick={this.handleSubmit.bind(this)}
+            //onClick={e => this.handleSubmit(e, this.state.query)}
+            href=""
+          >
+            <span
+              className="glyphicon glyphicon-search search"
+              aria-hidden="true"
+            />
+          </a>
+          <input type="submit" hidden />
+        </form>
       </div>
     );
   }
