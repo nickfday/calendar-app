82b5b26737e288c004399f4bee3f81c1359d80e5
Nick Finley-Day <nickfday@gmail.com>
2017-11-23 13:47:20 +0000

    Fixed console warnings

--- src/Components/Calendar/CalendarHelper.js
+++ src/Components/Calendar/CalendarHelper.js
@@ -1,19 +1,16 @@
-import React from 'react';
 import axios from 'axios';
-import { searchFilter, filterMultiSelect } from '../Misc/Helper';
-import * as env from '../../env';
 import moment from 'moment';
 
 export function APIFetch(path, type) {
   return axios.get(path).then(function(response) {
     //manipulateData(response.data);
-    if (type == 'list') {
+    if (type === 'list') {
       return {
         response: manipulateData(response.data),
         eventTypes: getListData(response.data, 'event_type'),
         audienceTypes: getListData(response.data, 'audience')
       };
-    } else if (type == 'single') {
+    } else if (type === 'single') {
       return {
         response: manipulateData(response.data)
         //singleEvent: fetchSingleEvent(manipulateData(response.data))
@@ -22,34 +19,23 @@ export function APIFetch(path, type) {
   });
 }
 
-function fetchSingleEvent(data) {
-  console.log(data);
-}
-
 function getListData(object, key) {
-  console.log('get list data');
   return getTypes(object, key);
-}
-
-function getSingleData(object) {
-  console.log('get single data');
 }
 
 function manipulateData(object) {
   // ToDo - Make into one function
   // Create types/audience in state
-  console.log('manipulate data');
-  console.log(object);
-  object.map(function(item) {
+  object.forEach(function(item) {
     let eventTypes = [];
-    item.event_type.split(', ').map(function(j) {
+    item.event_type.split(', ').forEach(function(j) {
       eventTypes.push(j);
     });
     item.event_type = eventTypes;
 
     let audienceTypes = [];
     if (item.audience) {
-      item.audience.split(', ').map(function(j) {
+      item.audience.split(', ').forEach(function(j) {
         audienceTypes.push(j);
       });
     }
@@ -78,33 +64,21 @@ function manipulateData(object) {
       formattedArray.push(Object.assign({}, item));
     });
   });
-  console.log(formattedArray);
-
   return formattedArray;
 }
-
-function stringToArray() {}
 
 // Generates a list of event/audience types so events can be filtered
 
 function getTypes(event, key) {
-  console.log('event is  ' + event);
-  console.log('key is ' + key);
-
   let keyTypes = [];
-  event.map(item => {
-    console.log('key is ' + key);
-    console.log(item);
-
+  event.forEach(item => {
     if (item[key]) {
       item[key].map(type => {
-        console.log('key is ' + key);
         if (keyTypes.indexOf(type) === -1) keyTypes.push(type);
         return false;
       });
       return false;
     }
   });
-  console.log(keyTypes);
   return keyTypes;
 }
