0a13aa2119bf825d991d3c5dc86e688ee50eea2b
Nick Finley-Day <nickfday@gmail.com>
2017-12-05 18:19:15 +0000

    updated env paths

--- src/env.js
+++ src/env.js
@@ -1,4 +1,7 @@
 export const API = {
-  domain: "http://finley-day.com",
+  domain: "http://wcc.dev",
   endPoint: "/api/calendar/views/calendar_json.json"
+ // wcc master settings: 
+ // domain: 'https://wcc-ce-master.codeenigma.net',
+ // endPoint: '/api/calendar/views/calendar_json.json'
 };
